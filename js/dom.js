bba91443b900611e8714bf430fc89e4f195a37bf
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-03-29 09:23:05 +0600

    js dom added css and js folder

---
+++ js/dom.js
@@ -0,0 +1,5 @@
+// console.log("hellow i am from js dom");
+const id = document.getElementById("place-title");
+console.log(id.innerHTML= "this is my favourite places");
+const fruits = document.getElementById("fruits-title");
+console.log(fruits.innerText);
